b7e7aadb31425d8bfd9028ea35f6f1237aadf050
NB1723 <notalia03@gmail.com>
2021-05-18 07:56:19 +0300

    lab2.3

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Complex.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			Complex A(5, 7);
+			Complex B(3, 5);
+
+			Complex C = A + B;
+			int S = C.GetRe();
+			Assert::AreEqual(S, 8);
+		}
+	};
+}
---
+++ lab2.3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "Complex.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "initialization of A" << endl;
+    Complex A(4, 5), B;
+    cout << "A = " << A << endl;
+
+    cout << "Size of class: " << sizeof(A) << endl;
+
+    cout << "initialization of B" << endl;
+    cin >> B;
+    cout << "B = " << B << endl;
+
+    cout << "Sum of complex numbers" << endl;
+    Complex C = A + B;
+    cout << "C = " << C << endl;
+
+    cout << "Product of complex numbers" << endl;
+    Complex D = A * B;
+    cout << "D = " << D << endl;
+
+    if (A == B)
+        cout << "Complex numbers are equal" << endl;
+    else
+        cout << "Complex numbers are not equal" << endl;
+
+    cout << "++A: " << ++A << endl;
+    cout << "--A: " << --A << endl;
+    cout << "A++: " << A++ << endl;
+    cout << "A--: " << A-- << endl << endl;
+}
